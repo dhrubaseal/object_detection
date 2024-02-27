014a2da267e89ecc874c20ee62e6efe777f9a249
dhrubaseal <dhrubaseal@gmail.com>
2024-02-27 11:38:22 +0530

    version_5

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ model = YOLO("yolov8n.yaml")  # build a new model from scratch
 model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
 
 # Use the model
-model.train(data="config.yaml", epochs=100)  # train the model
+model.train(data="config.yaml", epochs=20)  # train the model
 metrics = model.val()  # evaluate model performance on the validation set
 # results = model("C:\Personal_Projects\projects\Object Detection\object_detection\data\test\images\frame_0400.jpg")  # predict on an image
 path = model.export(format="onnx")  # export the model to ONNX format
