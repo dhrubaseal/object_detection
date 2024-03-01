e2b9da8054d422fc35bdad80a3675c7c9dd28de7
dhrubaseal <dhrubaseal@gmail.com>
2024-03-01 18:15:34 +0530

    version_4

--- .object_detection/Scripts/activate.bat
+++ .object_detection/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Personal_Projects\projects\Object Detection\object_detection\.object_detection
+set VIRTUAL_ENV=C:\Personal_Projects\projects\Computer Vision\object_detection\.object_detection
 
 if not defined PROMPT set PROMPT=$P$G
 
