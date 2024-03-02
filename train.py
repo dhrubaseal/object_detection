a92fb8cc957547b42ef79316013254bea3b2d91c
dhrubaseal <dhrubaseal@gmail.com>
2024-03-02 14:45:08 +0530

    version_2

--- train.py
+++ train.py
@@ -4,4 +4,4 @@ from ultralytics import YOLO
 model = YOLO("yolov8n.yaml")  # build a new model from scratch
 
 # Use the model
-results = model.train(data="config.yaml", epochs=1)  # train the model new
+results = model.train(data="config.yaml", epochs=1)  # train the model
